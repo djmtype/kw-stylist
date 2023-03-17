c0bad02445736659c5afb24cdea37d8afd10a1f6
Scott Rod <djmtype@gmail.com>
2023-03-17 10:12:01 -0400

    Added layout and og

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,18 @@
+import { siteMeta } from "./src/site.config";
 import { defineConfig } from 'astro/config';
 
+import image from "@astrojs/image";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: siteMeta.url,
+  // experimental: {
+  // 	assets: true
+  //  },
+  // image: {
+  //   service: "astro/assets/services/sharp"
+  // },
+  integrations: [image({
+		serviceEntryPoint: '@astrojs/image/sharp'
+	})]
+});
---
+++ src/site.config.ts
@@ -0,0 +1,6 @@
+export const siteMeta = {
+  title: "Katina Wilson • Stylist",
+  description: "When you need a hair stylist in the Grand Strand, SC area, call Katina Wilson at 336-398-4608",
+  url: "https://katinawilson.com/",
+  image: "/images/og-image.png"
+}
