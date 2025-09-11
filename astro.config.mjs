b7c0088dc0ac14fac7862e4056058a8a17f359ac
Scott Rod <scott@spinlinestudio.com>
2025-09-11 12:35:20 -0400

    Update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,9 @@
 import { siteMeta } from "./src/site.config";
 import { defineConfig } from 'astro/config';
 
-import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
   site: siteMeta.url,
-  // experimental: {
-  // 	assets: true
-  //  },
-  // image: {
-  //   service: "astro/assets/services/sharp"
-  // },
-  integrations: [image({
-		serviceEntryPoint: '@astrojs/image/sharp'
-	})]
+  
 });
