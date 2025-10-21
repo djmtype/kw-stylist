62171e13de5ddd5678677cf9060e33a58b96373d
Scott Rod <scott@spinlinestudio.com>
2025-10-21 15:22:01 -0400

    Update site.config.ts

--- src/site.config.ts
+++ src/site.config.ts
@@ -1,6 +1,6 @@
 export const siteMeta = {
   title: "Katina Wilson • Stylist",
   description: "When you need a hair stylist in the Grand Strand, SC area, call Katina Wilson at 336-398-4608",
-  url: "https://katinawilson.com/",
+  url: "https://thesuite-hair.com/",
   image: "/images/og-image.png"
 }
